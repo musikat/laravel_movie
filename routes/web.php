0cc28b402f9341e5cb264ac59ac7ed1f42151c68
musikat <harrisarmueladeleon08@gmail.com>
2023-12-11 16:10:26 +0800

    Login

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\PagesController;
 Route::get('/home',[PagesController::class,'index']);
 Route::get('/genre',[PagesController::class,'genre']);
 Route::get('/review',[PagesController::class,'review']);
-Route::get('/login',[PagesController::class,'login']);
+Route::get('/login1',[PagesController::class,'login1']);
 // End Front
 
 // Admin
